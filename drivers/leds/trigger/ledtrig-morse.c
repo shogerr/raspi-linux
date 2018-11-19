c96c5848e941187883f3ff0d68d9ea093a4395ef
dakota-osu <altond@oregonstate.edu>
2018-11-18 18:13:04 -0800

    adds character device with noop functions

--- drivers/leds/trigger/ledtrig-morse.c
+++ drivers/leds/trigger/ledtrig-morse.c
@@ -14,7 +14,39 @@
 #include <linux/sched/loadavg.h>
 #include <linux/leds.h>
 #include <linux/reboot.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/kdev_t.h>
+#include <linux/version.h>
+#include <linux/device.h>
+#include <linux/errno.h>
+#include <linux/uaccess.h>
+#include <linux/types.h>
+#include <linux/ioctl.h>
+
 #include "../leds.h"
+
+#define FIRST_MINOR 0
+#define MINOR_CNT 1
+
+static void led_morse_function(unsigned long data);
+
+static ssize_t led_speed_show(struct device* dev, struct device_attribute* attr, char* buf); 
+static ssize_t led_speed_store(struct device* dev, struct device_attribute* attr, const char* buf, size_t size); 
+
+static void morse_trig_activate(struct led_classdev* led_cdev);
+static void morse_trig_deactivate(struct led_classdev* led_cdev); 
+
+static int morse_reboot_notifier(struct notifier_block* nb, unsigned long code, void* unused);
+static int morse_panic_notifier(struct notifier_block* nb, unsigned long code, void* unused);
+
+static int __init morse_trig_init(void);
+static void __exit morse_trig_exit(void);
+
+static int on_open(struct inode* i, struct file* f);
+static int on_close(struct inode* i, struct file* f);
+static ssize_t on_read(struct file* f, char __user *buf, size_t size, loff_t* ppos);
+static ssize_t on_write(struct file* f, const char __user *buf, size_t size, loff_t* ppos);
 
 static const char* CHAR_TO_MORSE[128] = {
 	NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
@@ -36,13 +68,10 @@ static const char* CHAR_TO_MORSE[128] = {
 };
 
 static int panic_morse;
+static char* message;
 
 struct morse_trig_data {
-
-	// start with an 81 char message, 
-	// may have to change when it comes
-	// time to do project4
-	char message[10];	
+	// how fast should the morse be rendered
 	unsigned int speed;
 
 	// where in the message are we
@@ -55,7 +84,35 @@ struct morse_trig_data {
 	struct timer_list timer;
 };
 
-/** this function ddtermines the state of the led and how long it will 
+static DEVICE_ATTR(speed, 0644, led_speed_show, led_speed_store);
+
+static struct notifier_block morse_reboot_nb = {
+	.notifier_call = morse_reboot_notifier
+};
+
+static struct notifier_block morse_panic_nb = {
+	.notifier_call = morse_panic_notifier	
+};
+
+static struct led_trigger morse_led_trigger = {
+	.name = "morse",
+	.activate = morse_trig_activate,
+	.deactivate = morse_trig_deactivate
+};
+
+static dev_t dev;
+static struct cdev c_dev;
+static struct class* cl;
+
+static struct file_operations fops = {
+	.owner = THIS_MODULE,
+	.open = on_open,
+	.read = on_read,
+	.write = on_write,
+	.release = on_close
+};
+
+/** this function determines the state of the led and how long it will 
  *  be on for
  * 
  * Standard Morse code is defined to use dahs that are 3 times longer than the dits. 
@@ -105,7 +162,7 @@ static void led_morse_function(unsigned long data) {
 			break;
 		case '\0':
 			delay = 20 * dit;
-			morse_data->message_location = morse_data->message;	
+			morse_data->message_location = message;	
 			break;
 		default:
 			delay = 3 * dit;
@@ -144,14 +201,14 @@ static ssize_t led_speed_store(struct device* dev, struct device_attribute* attr
 	return size;
 }
 
-static DEVICE_ATTR(speed, 0644, led_speed_show, led_speed_store);
-
 static void morse_trig_activate(struct led_classdev* led_cdev) {
 	struct morse_trig_data* morse_data;
 	int rc;
-	char* message;
-	char *dst, *src;
 	
+	int ret;
+	struct device* dev_ret;
+
+	// morse trigger
 	morse_data = kzalloc(sizeof(*morse_data), GFP_KERNEL);
 	if(!morse_data)
 		return;
@@ -165,28 +222,42 @@ static void morse_trig_activate(struct led_classdev* led_cdev) {
 
 	setup_timer(&morse_data->timer, led_morse_function, (unsigned long)led_cdev);
 
-	// this will probably change as it seems like 
-	// this info is used to determine when the led is 
-	// to be on/off
+	// NOT IN USE 
 	message = "sos com";
-	dst = morse_data->message;
-	
-	for(src = message; *src != '\0'; src++) {
-		*dst = (*src);
-		dst++;
-	}
-
-	*dst = '\0';
-	printk(KERN_INFO "activate 2 message is %s\n", morse_data->message);
 
 	morse_data->phase = 0;
 	morse_data->speed = 1;
-	morse_data->message_location = morse_data->message;
+	morse_data->message_location = message;
 	morse_data->morse_location = CHAR_TO_MORSE[(int)(*morse_data->message_location)];
 
 	led_morse_function(morse_data->timer.data);
 	set_bit(LED_BLINK_SW, &led_cdev->work_flags);
 	led_cdev->activated = true;
+
+	// character device, should handle erros better (fail to activate this module?)
+	if((ret = alloc_chrdev_region(&dev, FIRST_MINOR, MINOR_CNT, "morse_chr_dev")) < 0) {
+		// fail
+	}
+
+	cdev_init(&c_dev, &fops);
+
+	if((ret = cdev_add(&c_dev, dev, MINOR_CNT)) < 0) {
+		// fail
+	}
+
+	if(IS_ERR(cl = class_create(THIS_MODULE, "char"))) {
+		cdev_del(&c_dev);
+		unregister_chrdev_region(dev, MINOR_CNT);
+		// fail
+	}
+
+	if(IS_ERR(dev_ret = device_create(cl, NULL, dev, NULL, "morse_chr_dev"))) {
+		class_destroy(cl);
+		cdev_del(&c_dev);
+		unregister_chrdev_region(dev, MINOR_CNT);
+		// fail
+	}
+
 }
 
 static void morse_trig_deactivate(struct led_classdev* led_cdev) {
@@ -198,14 +269,14 @@ static void morse_trig_deactivate(struct led_classdev* led_cdev) {
 		kfree(morse_data);
 		clear_bit(LED_BLINK_SW, &led_cdev->work_flags);
 		led_cdev->activated = false;
+
+		device_destroy(cl, dev);
+		class_destroy(cl);
+		cdev_del(&c_dev);
+		unregister_chrdev_region(dev, MINOR_CNT);
 	}
 }
 
-static struct led_trigger morse_led_trigger = {
-	.name = "morse",
-	.activate = morse_trig_activate,
-	.deactivate = morse_trig_deactivate
-};
 
 static int morse_reboot_notifier(struct notifier_block* nb, unsigned long code, void* unused) {
 	led_trigger_unregister(&morse_led_trigger);
@@ -216,15 +287,6 @@ static int morse_panic_notifier(struct notifier_block* nb, unsigned long code, v
 	panic_morse = 1;
 	return NOTIFY_DONE;
 }
-
-static struct notifier_block morse_reboot_nb = {
-	.notifier_call = morse_reboot_notifier
-};
-
-static struct notifier_block morse_panic_nb = {
-	.notifier_call = morse_panic_notifier	
-};
-
 static int __init morse_trig_init(void) {
 	int rc = led_trigger_register(&morse_led_trigger);
 
@@ -240,6 +302,22 @@ static void __exit morse_trig_exit(void) {
 	unregister_reboot_notifier(&morse_reboot_nb);
 	atomic_notifier_chain_unregister(&panic_notifier_list, &morse_panic_nb);
 	led_trigger_unregister(&morse_led_trigger);
+}
+
+static int on_open(struct inode* i, struct file* f) {
+	return 0;
+}
+
+static int on_close(struct inode* i, struct file* f) {
+	return 0;
+}
+
+static ssize_t on_read(struct file* f, char __user *buf, size_t size, loff_t* ppos) {
+	return 0;
+}
+
+static ssize_t on_write(struct file* f, const char __user *buf, size_t size, loff_t* ppos) {
+	return 0;
 }
 
 module_init(morse_trig_init);
