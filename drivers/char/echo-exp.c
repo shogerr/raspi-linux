32272c82e31e1e0a6afcdd4264d378b21dbf00c9
dakota-osu <altond@oregonstate.edu>
2018-11-23 17:53:07 -0800

    example for a buffer

--- drivers/char/echo-exp.c
+++ drivers/char/echo-exp.c
@@ -7,15 +7,70 @@
 #include <linux/errno.h>
 #include <linux/uaccess.h>
 #include <linux/types.h>
+#include <linux/slab.h>
+#include <linux/mutex.h>
+#include <linux/timer.h>
+#include <linux/sched.h>
+#include <linux/sched/loadavg.h>
 
 #define FIRST_MINOR 0
 #define MINOR_CNT 1
 
 #define DRIVER_NAME "echo-exp"
+#define BUFFER_SIZE 10
 
 static dev_t dev;
 static struct cdev c_dev;
 static struct class* cl;
+
+static struct timer_list timer;
+
+static DEFINE_MUTEX(lock);
+
+struct message_buffer {
+	char data[BUFFER_SIZE];
+
+	unsigned long size;
+	unsigned long next_in;
+	unsigned long next_out; 
+};
+
+static struct message_buffer m_buf;
+
+static void buffer_write(struct message_buffer* buf, char c) {
+	buf->data[buf->next_in] = c;
+	buf->next_in = (buf->next_in + 1) % BUFFER_SIZE;
+	buf->size++;
+}
+
+static char buffer_read(struct message_buffer* buf) {
+	char result = buf->data[buf->next_out];
+	buf->next_out = (buf->next_out + 1)  % BUFFER_SIZE;
+	buf->size--;
+	return result;
+}
+
+static void echo_function(unsigned long data) {
+	char local;
+	unsigned long delay = 70;
+
+	// printk("here\r\n");
+	if(m_buf.size) {
+		if(mutex_lock_interruptible(&lock)) {
+			// fail
+		}
+
+		if(m_buf.size)
+			local = buffer_read(&m_buf);
+
+		mutex_unlock(&lock);
+
+		printk("Recv: %c\r\n", local);
+	}
+
+	delay = msecs_to_jiffies(delay);
+	mod_timer(&timer, jiffies + msecs_to_jiffies(delay));
+}
 
 static int my_open(struct inode* i, struct file* f) {
 	printk("Echo Driver Open\r\n");
@@ -34,6 +89,7 @@ static ssize_t echo_read(struct file* file, char __user *buf, size_t size, loff_
 }
 
 static ssize_t echo_write(struct file* file, const char __user *buf, size_t size, loff_t* ppos) {
+#if 0
 	char local[11];
 	unsigned long buf_size;
 	
@@ -50,6 +106,28 @@ static ssize_t echo_write(struct file* file, const char __user *buf, size_t size
 	printk("Echo Driver Write %ld bytes: %s\r\n", size, local);
 
 	return min(size, buf_size - 1);
+#else
+	char local = '*';
+
+	if(m_buf.size == BUFFER_SIZE) 
+		return 0;
+
+	if(copy_from_user(&local, buf, min(size, (size_t)1))) {
+		return -EACCES;
+	}
+
+	printk("Echo Driver Write %ld bytes: %c\r\n", size, local);
+	
+	if(mutex_lock_interruptible(&lock)) 
+		return -ERESTARTSYS;
+
+	if(local != '*')
+		buffer_write(&m_buf, local);
+
+	mutex_unlock(&lock);
+
+	return min(size, (size_t)1);
+#endif
 }
 
 static struct file_operations fops = {
@@ -88,12 +166,25 @@ static int __init echo_init(void) {
 		return PTR_ERR(dev_ret);
 	}
 
+	// set up timer
+	setup_timer(&timer, echo_function, (unsigned long)NULL);
+	mod_timer(&timer, jiffies + msecs_to_jiffies(10));
+
+	// set up buffer
+	m_buf.size = 0;
+	m_buf.next_in = 0;
+	m_buf.next_out = 0;
+	memset(m_buf.data, 0, sizeof(m_buf.data));
+
 	printk("Echo Driver Has Been Loaded\r\n");
 
 	return 0;
 }
 
 static void __exit echo_exit(void) {
+
+	del_timer_sync(&timer);
+
 	device_destroy(cl, dev);
 	class_destroy(cl);
 
